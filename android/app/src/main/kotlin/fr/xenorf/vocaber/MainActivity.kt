9cec9d01b5e4f0a1fe1ca254ae6f1f5115794e87
Xenorf <90411648+Xenorf@users.noreply.github.com>
2026-06-30 20:21:46 +0200

    version bump + playstore edge to edge recos

--- android/app/src/main/kotlin/fr/xenorf/vocaber/MainActivity.kt
+++ android/app/src/main/kotlin/fr/xenorf/vocaber/MainActivity.kt
@@ -1,5 +1,13 @@
 package fr.xenorf.vocaber
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Enable edge-to-edge content rendering; Flutter UI should handle insets (SafeArea/MediaQuery)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
